8d2042dd11b54a440ee6f65b4fe57f42e6b76cc9
xDatter <demwl_14@hotmail.com>
2023-04-15 00:16:07 -0400

    formulario base listo, falta conectar bd

--- js/index.js
+++ js/index.js
@@ -1,31 +1,60 @@
 import { savetask } from './firebase.js'
-const openmoda1  =document.getElementById("open2")
+const openmoda1  =document.getElementById("abrirRegistrar")
 const modall = document.getElementById("modall")
-const cerrarregistrar =document.getElementById("cerrarregistrar")
+const cerrarRegistrar =document.getElementById("cerrarRegistrar")
 const registerForm = document.getElementById("register-form")
+const botonRegistrar = document.getElementById("botonRegistrar")
+const contenidoTabla = document.getElementById("contenidoTabla");
 
 
 const showRegistermodall = () =>{
     modall.classList.toggle("is-active")
 }
+// const volver = () =>{
+//     window.location.replace('./index.html');
+//     return false;
+// }
 
 openmoda1.addEventListener('click', showRegistermodall)
-cerrarregistrar.addEventListener('click',showRegistermodall)
+cerrarRegistrar.addEventListener('click',showRegistermodall)
+// registerForm.addEventListener('submit',volver)
+const campos=["rut","nombre","apellido","ciudad","direccion","telefono","email","fechaNac","estadocivil","comentario"]
+
+botonRegistrar.addEventListener('click',(e) =>{
+    console.log("aqui")
+    const fragment = new DocumentFragment();
+    const divRegistro = document.createElement("div");
+    divRegistro.classList.add("registroTabla");
+
+    for (let i=0 ; campos.length>i ;i++){
+
+        let divCampo = document.createElement("div");
+        let pCampo = document.createElement("p");
+        let texto = document.getElementById(`${campos[i]}`);
+        divCampo.classList.add("campo");
+
+        if(i == 6 || i == 7){
+            divCampo.classList.add("campMed");
+        }else if(i== 9){
+            divCampo.classList.add("campGrande");
+        }else{
+            divCampo.classList.add("campPeque");
+        }
+
+        pCampo.innerText = `${texto.value}`;
+        divCampo.append(pCampo);
+
+        divRegistro.append(divCampo);
+    };
+    console.log("hola");
+    console.log(divRegistro);
 
 
-registerForm.addEventListener('submit',(e) =>{
-    e.preventDefault()
-    const rut = registerForm['nombre'].value
-    const nombre = registerForm['nombre'].value
-    const apellido = registerForm['nombre'].value
-    const direccion = registerForm['nombre'].value
-    const ciudad = registerForm['nombre'].value
-    const telefono = registerForm['nombre'].value
-    const email = registerForm['nombre'].value
-    const date = registerForm['nombre'].value
-    const ecivil = registerForm['nombre'].value
-    const comentario = registerForm['nombre'].value
+    console.log(fragment);
+    fragment.append(divRegistro);
+    contenidoTabla.append(fragment);
+    
 
-    savetask(nombre.value)
+    // savetask(nombre.value)
 }
 )
