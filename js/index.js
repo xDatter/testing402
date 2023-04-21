bfc96dd09823dc2686c543a7ae0e2832dd4ca3f0
xDatter <demwl_14@hotmail.com>
2023-04-20 21:27:20 -0400

    staged

--- js/index.js
+++ js/index.js
@@ -5,11 +5,15 @@ import { guardarReg, traerRegistros, traerReg } from './firebase.js'
 
 
 const openmoda1 = document.getElementById("abrirRegistrar");
+const botonLimpiar = document.getElementById("botonLimpiar");
+const MostrarOnOff = document.getElementById("MostrarOnOff");
+const ejecutarbuscar = document.getElementById("ejecutarbuscar");
+const buscarInput = document.getElementById("buscarInput");
+
 const modall = document.getElementById("modall");
 const cerrarRegistrar = document.getElementById("cerrarRegistrar");
 const registerForm = document.getElementById("register-form");
 const contenidoTabla = document.getElementById("contenidoTabla");
-const botonLimpiar = document.getElementById("botonLimpiar");
 
 const camposGlobales = [
   "rut",
@@ -25,49 +29,104 @@ const camposGlobales = [
 ];
 
 
+
 window.addEventListener("DOMContentLoaded", async ()=>{
 
-  traerRegistros((querySnapshot) => {
 
-    let html = ``
-    querySnapshot.forEach((doc) => {
-      const registro = doc.data();
-
-
-      html +=`
-      <div class="registroTabla">
-        <div class="campo campPeque"><p>${registro.rut}</p></div>
-        <div class="campo campPeque"><p>${registro.nombre}</p></div>
-        <div class="campo campPeque"><p>${registro.apellido}</p></div>
-        <div class="campo campPeque"><p>${registro.ciudad}</p></div>
-        <div class="campo campPeque"><p>${registro.direccion}</p></div>
-        <div class="campo campPeque"><p>${registro.telefono}</p></div>
-        <div class="campo campMed"><p>${registro.email}</p></div>
-        <div class="campo campMed"><p>${registro.fechaNac}</p></div>
-        <div class="campo campPeque"><p>${registro.estadocivil}</p></div>
-        <div class="campo campGrande"><p>${registro.comentario}</p></div>
-      </div>
-      `;
-
-      // console.log(registro);
-    })
-    contenidoTabla.innerHTML = html;
-
-   
-    });
   });
 
 
 
-const showRegistermodall = async () => {
+const showRegistermodall = () => {
   modall.classList.toggle("is-active");
 
 };
 
+let mostrar="si";
+MostrarOnOff.textContent="Mostrar Registros";
+MostrarOnOff.addEventListener("click",(e)=>{
+  let html = ``;
+  
+  ////////////////
+  if (mostrar=="si"){
+    MostrarOnOff.textContent="Ocultar Registros";
+    MostrarOnOff.classList.toggle("is-danger");
+    MostrarOnOff.classList.toggle("is-info");
+    traerRegistros((querySnapshot) => {
+      querySnapshot.forEach((doc) => {
+        const registro = doc.data();
+  
+        html +=`
+        <div class="registroTabla" data-id="${doc.id}">
+          <div class="campo campPeque"><p>${registro.rut}</p></div>
+          <div class="campo campPeque"><p>${registro.nombre}</p></div>
+          <div class="campo campPeque"><p>${registro.apellido}</p></div>
+          <div class="campo campPeque"><p>${registro.ciudad}</p></div>
+          <div class="campo campPeque"><p>${registro.direccion}</p></div>
+          <div class="campo campPeque"><p>${registro.telefono}</p></div>
+          <div class="campo campMed"><p>${registro.email}</p></div>
+          <div class="campo campMed"><p>${registro.fechaNac}</p></div>
+          <div class="campo campPeque"><p>${registro.estadocivil}</p></div>
+          <div class="campo campGrande"><p>${registro.comentario}</p></div>
+        </div>
+        `;
+  
+      })
+      contenidoTabla.innerHTML = html;
+    });
+    mostrar = "no";
+  }else{
+    MostrarOnOff.textContent="Mostrar Registros";
+    MostrarOnOff.classList.toggle("is-danger");
+    MostrarOnOff.classList.toggle("is-info");
+    contenidoTabla.innerHTML = "";
+    mostrar = "si";
+  }
+    /////////////////
+});
+
+ejecutarbuscar.addEventListener("click", async ()=>{
+  const valorBuscar = String(buscarInput.value).toUpperCase()
+  //////////////////////////////7
+
+  let registrosMatch = [];
+
+  const regBuscar= await traerReg();
+  regBuscar.forEach((doc)=>{
+
+   const valor = doc.data();      //valor de la base de datos
+   const idMatch = doc.id;        // id del registro en la base de datos
+
+   let hayMAtch = "";
+
+   const arregloMatch =[String(valor.rut).toUpperCase(), String(valor.nombre).toUpperCase(), String(valor.apellido).toUpperCase(), String(valor.ciudad).toUpperCase(), String(valor.direccion).toUpperCase(), String(valor.telefono).toUpperCase(), String(valor.email).toUpperCase(), String(valor.fechaNac).toUpperCase(), String(valor.estadocivil).toUpperCase()];
+   
+   for( let i=0 ; i<arregloMatch.length ; i++){
+    if( valorBuscar == arregloMatch[i]){
+      hayMAtch += "1"
+    }
+
+  }
+  
+  if(hayMAtch != ""){
+    registrosMatch.push(idMatch);
+  }
+
+  });
+  
+  console.log(registrosMatch);
+
+  /////////////////
+  buscarInput.value = "";
+});
+
+
+
+
 botonLimpiar.addEventListener("click",(e)=>{
   registerForm.reset();
   registroOK("Se limpió formulario", "#00666b")
-})
+});
 
 registerForm.addEventListener("submit",async (evento) =>{
   evento.preventDefault();
